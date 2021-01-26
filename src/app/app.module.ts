cd22f620797693ebc6703c82f5b02d4746e6f5e2
Mechztcc <dojathan@gmail.com>
2021-01-26 14:50:31 -0300

    Created base idea to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './template/header/header.component';
+import { NoticeCardComponent } from './template/notice-card/notice-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NoticeCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/template/notice-card/notice-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notice-card',
+  templateUrl: './notice-card.component.html',
+  styleUrls: ['./notice-card.component.css']
+})
+export class NoticeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
